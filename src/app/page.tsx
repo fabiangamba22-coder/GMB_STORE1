b89ac85ac59b2037d94cc091bfd90ebe01b819a9
fabiangamba22-coder <fabiangamba22@gmail.com>
2026-03-25 19:15:55 -0500

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main style={{textAlign: "center", padding: "50px"}}>
+      <h1>GMB STORE 🛒</h1>
+      <p>Bienvenido a mi tienda virtual</p>
+      <button style={{
+        padding: "15px 30px",
+        fontSize: "18px",
+        background: "black",
+        color: "white",
+        border: "none",
+        borderRadius: "10px"
+      }}>
+        Ver productos
+      </button>
+    </main>
+  );
+}
